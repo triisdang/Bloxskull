7fa70634d92f176e7a1e0f9c0f8893e0e5efc26a
Tr1 <roroxji.jk.k5d678@bumpmail.io>
2025-03-29 16:47:49 +0700

    i think this better

--- src/yay/update.py
+++ src/yay/update.py
@@ -13,7 +13,10 @@ latest_version = get_latest_release()
 # local_version = localversion()
 local_version = "0.0.0" # test this 
 
-if latest_version and latest_version != local_version:
-    print(f"New version available! The latest version is : {latest_version} (Current: {local_version})")
-else:
-    print("App is up to date!")
+def newupdate() :
+    latest_version = get_latest_release()
+    local_version = "0.0.0"
+    if latest_version and latest_version != local_version:
+        return("True")
+    else:
+        print("False")
